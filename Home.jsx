3d1d572df33c2044260822f5543b4a905f0960cf
KonnetiPoojaSri <103838592+KonnetiPoojaSri@users.noreply.github.com>
2024-06-09 18:08:11 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"; // Import the CSS file
+
+function Home() {
+    return (
+        <div>
+            <h1 className="highlight">We got your mail and password we will get back to you asap</h1>
+        </div>
+    );
+}
+
+export default Home;
